81a42abd3a7530d90d60b6abad84d39301337560
Dong Hee Kim <opklnm102@gmail.com>
2018-06-30 19:10:20 +0900

    initial commit

---
+++ src/main/java/kr/co/mashup/mapc/MapcApplication.java
@@ -0,0 +1,12 @@
+package kr.co.mashup.mapc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapcApplication.class, args);
+    }
+}
---
+++ src/test/java/kr/co/mashup/mapc/MapcApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.co.mashup.mapc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MapcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
