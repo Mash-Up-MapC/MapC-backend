644a158080a9d65d0076725794ed74adf5604773
KiWooyoung <ueong777@gmail.com>
2018-08-23 02:42:52 +0900

    [34][ADD] DTO 추가

---
+++ src/main/java/kr/co/mashup/mapc/dto/CourseDto.java
@@ -0,0 +1,39 @@
+package kr.co.mashup.mapc.dto;
+
+import kr.co.mashup.mapc.entity.Course;
+import kr.co.mashup.mapc.entity.Price;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+public class CourseDto {
+
+    @Data
+    @ToString
+    @Slf4j
+    public static class CourseSelection {
+        private long id;
+        private String courseName;
+        private List<String> price;
+        private List<Price.PassengerType> passengerType;
+
+        public CourseSelection(Course entity) {
+            this.id = entity.getId();
+            this.courseName = entity.getCourseName();
+            price = new ArrayList<>();
+            for (Price p : entity.getPriceItems()) {
+                price.add(p.getPrice());
+            }
+            passengerType = new ArrayList<>();
+            for (Price pp : entity.getPriceItems()) {
+                passengerType.add(pp.getPassengerType());
+            }
+        }
+    }
+}
