6d37a7f4854b2345c021e8e95f2c8299765c53c6
KiWooyoung <ueong777@gmail.com>
2018-08-24 23:15:19 +0900

    [#34][ADD] station_image 맵핑

--- src/main/java/kr/co/mashup/mapc/entity/StationImage.java
+++ src/main/java/kr/co/mashup/mapc/entity/StationImage.java
@@ -1,4 +1,26 @@
 package kr.co.mashup.mapc.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@Entity
+@ToString
+@Table(name = "station")
 public class StationImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "station_id")
+    private Station station;
+
+    @Column(name = "image_url")
+    private String imageUrl;
 }
