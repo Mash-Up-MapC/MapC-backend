afc0a362f17ba475bb391a0add298c0d2d626021
Dong Hee Kim <opklnm102@gmail.com>
2018-08-31 02:11:40 +0900

    [#34][ADD] Add JpaAuditing configuration

--- src/main/java/kr/co/mashup/mapc/MapcApplication.java
+++ src/main/java/kr/co/mashup/mapc/MapcApplication.java
@@ -2,11 +2,18 @@ package kr.co.mashup.mapc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.convert.Jsr310Converters;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.time.ZoneOffset;
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan(basePackages = "kr.co.mashup.mapc", basePackageClasses = Jsr310Converters.class)
+@EnableJpaRepositories(basePackages = "kr.co.mashup.mapc")
+@EnableJpaAuditing
 public class MapcApplication {
 
     public static void main(String[] args) {
