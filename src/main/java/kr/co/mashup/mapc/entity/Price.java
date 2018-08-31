5406bd3ce92e7985c988f5bb8bb25504867f00ed
korea8378 <korea8378@naver.com>
2018-09-01 02:12:10 +0900

    [#25][ADD] course detail mock api

---
+++ src/main/java/kr/co/mashup/mapc/entity/Price.java
@@ -0,0 +1,34 @@
+package kr.co.mashup.mapc.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@Entity
+@ToString
+@Table(name = "price")
+public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @Column(name = "price")
+    private String price;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "passenger_type")
+    private PassengerType passengerType;
+
+    public enum PassengerType {
+        ADULT,
+        CHILD
+    }
+}
