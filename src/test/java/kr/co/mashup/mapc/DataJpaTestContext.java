6dfe7739971a44c9004bd86bb72fa39462282a47
Dong Hee Kim <opklnm102@gmail.com>
2018-09-02 18:06:19 +0900

    [#34][FIX] develop merge시 생성된 불필요 파일 제거 및 모든 TC 성공하도록 수정

--- src/test/java/kr/co/mashup/mapc/DataJpaTestContext.java
+++ src/test/java/kr/co/mashup/mapc/DataJpaTestContext.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @DataJpaTest
-public class DataJpaTestContext {
+public abstract class DataJpaTestContext {
 
     @Autowired
     protected TestEntityManager testEntityManager;
