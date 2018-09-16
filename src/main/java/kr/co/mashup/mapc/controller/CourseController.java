121e03af7b9a65838f535dd8efb11b52da8af947
korea8378 <korea8378@naver.com>
2018-09-17 02:12:05 +0900

    [#56][ADD] update controller, service

--- src/main/java/kr/co/mashup/mapc/controller/CourseController.java
+++ src/main/java/kr/co/mashup/mapc/controller/CourseController.java
@@ -4,10 +4,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import kr.co.mashup.mapc.dto.DetailCourse;
-import kr.co.mashup.mapc.dto.TouristAttractions;
+import kr.co.mashup.mapc.service.CourseService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,13 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/courses", produces = "application/json")
 public class CourseController {
 
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
 
     @ApiOperation(value = "관광지 리스트 더 보기", notes = "관광지 리스트를 더 조회 한다.")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "조회 성공")
     })
-    @GetMapping(path = "/courses/{course_Id}/{page}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<String> getMoreTourist(@PathVariable(value = "course_Id") String courseId) {
+    @GetMapping(path = "/{course_Id}/{page}")
+    public ResponseEntity<Long> getMoreTouristSpot(
+            @PathVariable(value = "course_Id") Long courseId, @PathVariable(value = "page") int page) {
+        courseService.getMoreTouristSpot(courseId, page);
         return new ResponseEntity<>(courseId, HttpStatus.OK);
     }
 }
--- src/main/java/kr/co/mashup/mapc/service/CourseService.java
+++ src/main/java/kr/co/mashup/mapc/service/CourseService.java
@@ -1,9 +1,20 @@
 package kr.co.mashup.mapc.service;
 
 
+import kr.co.mashup.mapc.repository.StationRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CourseService {
+
+    private final StationRepository stationRepository;
+
+    public CourseService(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
+
+    public void getMoreTouristSpot(Long courseId, int page) {
+
+    }
 
 }
